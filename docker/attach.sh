a8b532ac3fd80354b6939752db02a15f5834f08e
Hasan Korre <hkorre@gmail.com>
2021-05-28 15:44:14 -0700

    hkorre: copy docker files from other repo

---
+++ docker/attach.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec -it hk_AdNer /bin/bash
---
+++ docker/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install htop
+sudo apt-get -y install docker.io
+sudo usermod -aG docker $(whoami)
+
+docker build --tag hkorre:AdNer .
